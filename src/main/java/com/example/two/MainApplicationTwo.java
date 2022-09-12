31acad44db53fbfceccdd1009272c788706d51ce
Richard Ward <RichieS@GMail.com>
2022-09-12 16:59:36 +0100

    Fix second solution

--- src/main/java/com/example/two/MainApplicationTwo.java
+++ src/main/java/com/example/two/MainApplicationTwo.java
@@ -3,6 +3,6 @@ package com.example.two;
 public class MainApplicationTwo {
     public static void main(String[] args) {
         final var service = new SolutionTwoService();
-        System.out.println(service.run(new int[]{1, 2, 3, 4}));
+        System.out.println(service.run(new int[]{1,2,-40,4}));
     }
 }
--- src/main/java/com/example/two/SolutionTwoService.java
+++ src/main/java/com/example/two/SolutionTwoService.java
@@ -1,10 +1,12 @@
 package com.example.two;
 
+import static java.lang.Integer.MAX_VALUE;
+
 public class SolutionTwoService {
     public int run(int[] A) {
-        int ans = 0;
-        for (int i = 1; i < A.length; i++) {
-            if (ans > A[i]) {
+        int ans = MAX_VALUE;
+        for (int i = 0; i < A.length; i++) {
+            if (A[i] < ans) {
                 ans = A[i];
             }
         }
