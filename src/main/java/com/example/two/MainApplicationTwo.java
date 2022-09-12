449b0cbc4badd05491b20cfbafe7da84eb5935b7
Richard Ward <RichieS@GMail.com>
2022-09-12 16:47:36 +0100

    Set up second solution

--- src/main/java/com/example/two/MainApplicationTwo.java
+++ src/main/java/com/example/two/MainApplicationTwo.java
@@ -2,16 +2,7 @@ package com.example.two;
 
 public class MainApplicationTwo {
     public static void main(String[] args) {
-        System.out.println("Testing");
-    }
-
-    int solution(int[] A) {
-        int ans = 0;
-        for (int i = 1; i < A.length; i++) {
-            if (ans > A[i]) {
-                ans = A[i];
-            }
-        }
-        return ans;
+        final var service = new SolutionTwoService();
+        System.out.println(service.run(new int[]{1, 2, 3, 4}));
     }
 }
---
+++ src/main/java/com/example/two/SolutionTwoService.java
@@ -0,0 +1,13 @@
+package com.example.two;
+
+public class SolutionTwoService {
+    public int run(int[] A) {
+        int ans = 0;
+        for (int i = 1; i < A.length; i++) {
+            if (ans > A[i]) {
+                ans = A[i];
+            }
+        }
+        return ans;
+    }
+}
